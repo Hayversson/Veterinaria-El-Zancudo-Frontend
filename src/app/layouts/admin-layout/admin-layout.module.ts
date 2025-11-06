a0024bca6c78723011cb63542bc45e840c61a530
Hayversson <hayversson10one@gmail.com>
2025-11-06 18:57:15 -0500

    Se eliminó entidades no usadas y se agregó el método buscar.

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -7,15 +7,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { AnimalesComponent } from '../../animales/animales.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
-import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
 import { ChartsModule } from 'ng2-charts';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrModule } from 'ngx-toastr';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
 
 @NgModule({
   imports: [
@@ -30,12 +24,6 @@ import { UpgradeComponent } from '../../upgrade/upgrade.component';
   declarations: [
     DashboardComponent,
     UserProfileComponent,
-    TableListComponent,
-    UpgradeComponent,
-    TypographyComponent,
-    IconsComponent,
-    MapsComponent,
-    NotificationsComponent,
     AnimalesComponent
   ]
 })
