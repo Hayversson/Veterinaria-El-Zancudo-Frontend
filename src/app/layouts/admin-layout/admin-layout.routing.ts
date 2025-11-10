7b4e67dd55ee84d5cd1799230916d310f58ccb92
lincoln <lincolnandres988@gmail.com>
2025-11-10 16:39:40 -0500

    feat:clase factura en el dashboard del frontend

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { AnimalesComponent } from '../../animales/animales.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
+import { FacturaComponent } from '../../factura/factura.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'animales',        component: AnimalesComponent },
-    { path: 'user-profile',   component: UserProfileComponent }
+    { path: 'user-profile',   component: UserProfileComponent },
+    { path: 'factura',        component: FacturaComponent }
 ];
